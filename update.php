3b2604ce41287c02e5bcf63e6c16dc9ffdb5a62c
Thomas Skerbis <thomas.skerbis@mac.com>
2025-11-16 20:02:33 +0100

    Add update.php to run database migrations

---
+++ update.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * GitHub Installer - Update
+ * 
+ * Führt die install.php aus, um Tabellen-Updates durchzuführen
+ */
+
+// install.php ausführen für Tabellen-Updates
+require_once __DIR__ . '/install.php';
